40f23ac22e514e90b34e9c852a21d45425370e97
Kevin Winarko <kevinwinarko98@gmail.com>
2020-06-11 13:36:09 +0700

    update factory excel file for register and history

--- src/test/java/testCases/history/Factory_History_Details.java
+++ src/test/java/testCases/history/Factory_History_Details.java
@@ -15,6 +15,6 @@ public class Factory_History_Details extends TestBase{
 	
 	@DataProvider(name="dp")
 	public String[][] dataProvider() throws IOException {
-		return getExcelData("../DanaPulsaAutomationTest/src/test/java/testCases/history/historyDetailsTestData.xlsx");
+		return getExcelData("../DanaPulsaAutomationTest/src/test/java/testCases/history/historyTestData.xlsx", "Details");
 	}
 }
